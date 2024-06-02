cbd07e27dc2a2d426ee13c126f534b484754e8ec
Mikava <awvvak@gmail.com>
2024-06-02 20:04:19 +0300

    3

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -3,9 +3,12 @@ from posts.models import Post, Group
 
 
 class PostSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(read_only=True)
+
     class Meta:
         model = Post
         fields = ('id', 'text', 'pub_date', 'author')
+
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from rest_framework import viewsets
 from .serializers import PostSerializer, GroupSerializer
   # Импортировали класс Response
 from rest_framework import generics
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
 
 from posts.models import Post, Group
 
@@ -11,6 +14,21 @@ class PostList(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def perform_destroy(self, serializer):
+        if serializer.author != self.request.user:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        else:
+            serializer.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+    # def perform_destroy(self, request, pk):
+    #     post = get_object_or_404(Post, id=pk)
+    #     if request.user != post.author:
+    #         return Response(status=status.HTTP_403_FORBIDDEN)
+        
 
 class GroupList(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
