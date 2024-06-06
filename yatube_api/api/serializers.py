7a29f386a39238aa24507d66b3cd0f1bf6457500
Mik <awvvak@gmail.com>
2024-06-06 18:07:09 +0300

    4

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -4,11 +4,14 @@ from posts.models import Comment, Group, Post
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField(read_only=True)
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
 
     class Meta:
         model = Post
-        fields = ('id', 'text', 'pub_date', 'author')
+        fields = '__all__'
 
 
 class GroupSerializer(serializers.ModelSerializer):
@@ -16,12 +19,18 @@ class GroupSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Group
-        fields = ('id', 'title', 'slug', 'description')
+        fields = '__all__'
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField(read_only=True)
-    post = serializers.PrimaryKeyRelatedField(read_only=True)
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+    post = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='id'
+    )
 
     class Meta:
         model = Comment
